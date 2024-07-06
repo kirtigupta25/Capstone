1325e626ecf27eaac1918aede1c43e3a3612ad1d
Akaur54 <akaur5_be21@thapar.edu>
2024-07-06 11:18:58 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Landingpage/Home';
 
 function App() {
   return (
-    <p>hello</p>
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
